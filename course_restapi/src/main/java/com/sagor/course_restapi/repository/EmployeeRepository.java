fdc0f16587d9c2df4e6e7711aaa1ed3c4c10850d
Sagor Chandro <117623409+sagorChndro@users.noreply.github.com>
2022-11-19 17:25:18 +0600

    Delete course_restapi directory

--- course_restapi/src/main/java/com/sagor/course_restapi/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sagor.course_restapi.repository;
-
-
-import com.sagor.course_restapi.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
