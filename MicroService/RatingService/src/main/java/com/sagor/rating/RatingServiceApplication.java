b8ace9ee6f87da01a44ed40b0db88d019a6b6c8c
Sagor Chandro <117623409+sagorChndro@users.noreply.github.com>
2022-12-25 02:06:16 +0600

    Add files via upload

--- MicroService/RatingService/src/main/java/com/sagor/rating/RatingServiceApplication.java
+++ MicroService/RatingService/src/main/java/com/sagor/rating/RatingServiceApplication.java
@@ -2,8 +2,10 @@ package com.sagor.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
