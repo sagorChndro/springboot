715445c465176b033f5ea5d69b40f633e8110dab
Sagor Chandro <117623409+sagorChndro@users.noreply.github.com>
2022-12-24 17:45:49 +0600

    MicroService

---
+++ HotelService/src/test/java/com/sagor/hotel/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagor.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
