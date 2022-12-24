10620d5f60afe0a3e029fb90f3096ff1e60f3fef
Sagor Chandro <117623409+sagorChndro@users.noreply.github.com>
2022-12-25 02:09:32 +0600

    Add files via upload

---
+++ MicroService/ServiceRegistry/src/test/java/com/sagor/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagor.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
